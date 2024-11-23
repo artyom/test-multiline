1a90707b961e7adb48aff5b7455dfb1c3bebfd11
Artyom Pervukhin <github@artyom.dev>
2024-11-23 17:28:47 +0100

    Parse out input from environment

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"flag"
 	"fmt"
+	"iter"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -26,6 +28,16 @@ func run(args []string) error {
 		fmt.Printf("%d\t%q\n", i, s)
 	}
 	fmt.Println(groupEnd)
+	fmt.Println(groupStart, "Explicit handling of input three")
+	for line := range Lines(os.Getenv("INPUT_THREE")) {
+		fmt.Printf("raw value: %q\n", line)
+		if k, v, ok := strings.Cut(line, "="); ok {
+			k = strings.TrimSpace(k)
+			v = strings.TrimSpace(v)
+			fmt.Printf("as key-value pair: %s: %s\n", k, v)
+		}
+	}
+	fmt.Println(groupEnd)
 	return nil
 }
 
@@ -33,3 +45,20 @@ const (
 	groupStart = "::group::"
 	groupEnd   = "::endgroup::"
 )
+
+// TODO: replace with strings.Lines once Go 1.24 is out
+func Lines(s string) iter.Seq[string] {
+	return func(yield func(string) bool) {
+		for len(s) > 0 {
+			var line string
+			if i := strings.IndexByte(s, '\n'); i >= 0 {
+				line, s = s[:i+1], s[i+1:]
+			} else {
+				line, s = s, ""
+			}
+			if !yield(line) {
+				return
+			}
+		}
+	}
+}
